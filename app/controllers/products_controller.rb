335c589252d15257121645982cc60ca075cff9e3
benkivuva <benkivuvak5@gmail.com>
2026-01-15 13:31:08 +0300

    feat: implement product crud with turbo frames

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,67 @@
+class ProductsController < ApplicationController
+  before_action :set_product, only: %i[ show edit update destroy ]
+
+  # GET /products
+  def index
+    @products = Product.all.order(created_at: :desc)
+  end
+
+  # GET /products/1
+  def show
+  end
+
+  # GET /products/new
+  def new
+    @product = Product.new
+  end
+
+  # GET /products/1/edit
+  def edit
+  end
+
+  # POST /products
+  def create
+    @product = Product.new(product_params)
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to products_path, notice: 'Product was successfully created.' }
+        format.turbo_stream
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /products/1
+  def update
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html { redirect_to products_path, notice: 'Product was successfully updated.' }
+        format.turbo_stream
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /products/1
+  def destroy
+    @product.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to products_path, notice: 'Product was successfully destroyed.' }
+      format.turbo_stream
+    end
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :quantity, :low_stock_threshold)
+  end
+end
